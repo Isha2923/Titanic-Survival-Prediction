e9c87c9899858f1fa7c49ad17938298f55c10fcf
ISHA SHARMA <122515895+Isha2923@users.noreply.github.com>
2023-09-08 21:00:30 +0530

    Add files via upload

---
+++ titanicrepo.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pickle
+
+st.title("TITANIC SURVIVAL PREDICTION")
+
+pclass= st.number_input('Enter pclass(1,2,3)',min_value=1, max_value=3) 
+
+sex= st.radio('FEMALE/MALE',[0,1])   #female is 0 ,male is 1
+
+age=st.number_input('Enter age')
+
+sibsp= st.number_input('Sibsp (0,1,2,3)',min_value=0, max_value=3) #no. of siblings
+
+parch= st.number_input('Parch (0,1,2)',min_value=0, max_value=2) #no. of parents
+
+fare = st.number_input('Enter Fare') 
+
+
+ 
+clicked=st.button('Get prediction')
+print(clicked)   
+
+#LOADING THE MODEL
+with open('model.pkl', 'rb') as f:
+    model= pickle.load(f)     
+    
+if clicked==True:  
+    #take the data and predict survived/dead.
+     data=[pclass,sex,age,sibsp,parch,fare]  
+     print(data)        
+
+     pred = model.predict([data])[0] 
+     if (pred==0):
+        st.header("NOT SURVIVED")
+     else:
+        st.header("SURVIVED")
+
+
+
+
+
+
